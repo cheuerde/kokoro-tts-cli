3794063bdc74043236a10b848786a0e2697953c5
cheuerde <claasheuer@googlemail.com>
2025-01-15 15:24:56 +0100

    server

--- setup.py
+++ setup.py
@@ -3,21 +3,22 @@ from setuptools import setup, find_packages
 setup(
     name="kokoro-tts-cli",
     version="0.1.0",
+    author="Claas Heuer",
+    author_email="claasheuer@googlemail.com",
+    description="A CLI for Kokoro TTS with streaming and voice mixing capabilities",
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'kokoro-tts=kokoro_tts_cli.streamer:main',
-            'kokoro-tts-server=kokoro_tts_cli.server:run_server',
-            'kokoro-tts-client=kokoro_tts_cli.client_cli:run_client',
-        ],
-    },
     install_requires=[
-        "torch>=2.0.0",
-        "tqdm",
+        "numpy",
+        "torch",
         "sounddevice",
         "scipy",
-        "numpy",
-        "phonemizer",
-        "transformers",
     ],
+    entry_points={
+        'console_scripts': [
+            'kokoro-tts=kokoro_tts_cli.streamer:main',  # existing CLI
+            'kokoro-tts-server=kokoro_tts_cli.server:run_server',  # new server
+            'kokoro-tts-client=kokoro_tts_cli.client_cli:run_client',  # new client
+        ],
+    },
+    python_requires='>=3.7',
 )
